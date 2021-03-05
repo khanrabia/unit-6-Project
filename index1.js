f6ee3b1a45427594651373e8231f4e008cfe70ae
khanrabia <khan.rab34@gmail.com>
2021-03-04 21:58:06 -0500

    SKATEBOARD DONE

--- index1.js
+++ index1.js
@@ -1,8 +1,13 @@
-let spoonToken = "bb2c13a45bc54689836b718105638ae1";
+let spoonToken = "";
 
 document.addEventListener("DOMContentLoaded", () => {
     let form = document.getElementById("search-form");
     form.addEventListener("submit", callBack);
+
+    let moreResults = document.getElementById("more-results");
+    moreResults.addEventListener("click", similarRecipes);
+    
+    
 })
 
 // fetches the recipe that user puts in the input box
@@ -20,6 +25,7 @@ function callBack(e){
     fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${recipes}&tags=${diet}&addRecipeNutrition=true&apiKey=${spoonToken}&addRecipeInformation=true&instructionsRequired=true`, options)
     .then(res => res.json())
     .then(data => {
+        console.log(data)
         displayRecipe(data);
     })
 }
@@ -37,8 +43,7 @@ function displayRecipe(data){
         let li = document.createElement("li");
         li.innerText = `${recipe.title}`;
         ul.append(li);
-        let moreResults = document.getElementById("more-results");
-        moreResults.addEventListener("click", similarRecipes(recipe));
+        
         
         //hides the recipe list & displays the recipe instructions, picture & info
         li.addEventListener('click', () =>{
@@ -55,7 +60,7 @@ function displayRecipe(data){
             displayNutrientsFacts(recipe);
             displayIngredients(recipe);
             displaySteps(recipe);
-        });
+        });   
     });
 }
 
@@ -97,44 +102,73 @@ function displayIngredients(recipe){
         let li = document.createElement("li");
         li.innerText = `${ingredient["name"]} ${ingredient["amount"]} ${ingredient["unit"]}`;
         ulOfIngredients.append(li);
+        
     });
 }
-
+let id;
 // Steps of recipe
 function displaySteps(recipe){
     let steps = document.getElementById("steps"); // div of steps
     let ulOfSteps = document.getElementById("list-of-steps");
     let secondInstructions = recipe.analyzedInstructions[1];
     steps.style.display = "block";
-    
+    id = recipe.id
+    console.log(id)
     recipe.analyzedInstructions[0].steps.forEach(step => {
         let li = document.createElement("li");
         li.innerText = step["step"];
         ulOfSteps.append(li);
+        
     });
+    
     if (secondInstructions !== undefined) {
         recipe.analyzedInstructions[1].steps.forEach(step => {
             let li = document.createElement("li");
             li.innerText = step["step"];
             ulOfSteps.append(li);
         });
+        
     }
+    
+    
 }
 
 //Get Similar Recipes
-function similarRecipes(recipe){
+function similarRecipes(){
+    console.log("clicked")
+    
     let options = {
         headers: {
             'Content-Type': 'application/json'
         },
     }
-    fetch(`https://api.spoonacular.com/recipes/1165787/similar?apiKey=${spoonToken}`, options)
+    fetch(`https://api.spoonacular.com/recipes/${id}/similar?apiKey=${spoonToken}&addRecipeInformation=true&number=20`, options)
     .then(response => {
-        response.json()
-        console.log(response)
+        return response.json()
+        
     })
     .then(data =>{
-        console.log(data)
+        displaySimilarRecipe(data)
     })
 }
 
+function displaySimilarRecipe(data){
+    console.log(data)
+    let recipeList = document.getElementById("similar-recipes");
+    let recipeTitle = document.getElementById("recipe-title");
+    let singleRecipe = document.getElementById("single-recipe");
+    let ul = document.getElementById("similar-list");
+    recipeList.style.display = "block";
+    singleRecipe.style.display = "none";
+
+    // let head = document.getElementById("head-of-similar-recipes");
+    // head.innerText = `Here are similaer recipes to ${recipe.title}`
+    
+    data.forEach(recipe => {
+        console.log(recipe)
+        let li = document.createElement("li");
+        li.innerHTML = `<a target="blank" href =${recipe.sourceUrl}>${recipe.title}</a>`;
+        ul.append(li);
+        
+    });
+}
